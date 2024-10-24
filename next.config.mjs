6cf9da939283eff295413244f361551838e9805a
Adarsh Kumar <adarshkumar9315@gmail.com>
2024-10-25 03:54:20 +0530

    vercel.json added

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,  // Enables React strict mode to catch potential issues early
-    experimental: {
-      appDir: true,  // Enables the use of the new `app` directory for Next.js
-    },
+    reactStrictMode: true,
     // Optional: Customize Webpack if needed (e.g., path alias for easier imports)
     webpack: (config) => {
       config.resolve.alias['@'] = new URL('./src', import.meta.url).pathname;  // Adds an alias for the `src` directory
